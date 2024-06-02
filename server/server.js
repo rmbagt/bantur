183c941bdaa69dcc20871f9f19cf6974b104bcc9
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-06-02 10:00:13 +0700

    build: change vercel json

--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
 const fs = require('fs')
-const path = require('path')
-const filePath = path.join('/db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
 const router = jsonServer.router(db)
 
 // Comment out to allow write operations
